4604ba1920b29831270346f1d1d32a6f65bac1b8
Narayan mishra <83896806+narayanmanish@users.noreply.github.com>
2021-05-10 23:39:04 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Songs.com</title>
+
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+ <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-wEmeIV1mKuiNpC+IOBjI7aAzPcEZeedi5yW5f2yOq55WWLwNGmvvx4Um1vskeMj0" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-p34f1UUtsS3wqzfto5wAAmdvj+osOnFyQFpp4Ua3gs/ZVWx6oOypYoCJhGGScy+8" crossorigin="anonymous"></script>
+  
+ 
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Songs</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarColor01">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Dashboard
+            <span class="visually-hidden">(current)</span>
+          </a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Artist</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">View Artist</a>
+            <a class="dropdown-item" href="#">Add Artist</a>
+            
+          </div>
+        </li>
+       
+         <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Album</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">View Album</a>
+            <a class="dropdown-item" href="#">Add Album</a>
+            
+          </div>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Song</a>
+          <div class="dropdown-menu">
+            <a class="dropdown-item" href="#">View Songs</a>
+            <a class="dropdown-item" href="#">Add Songs</a>
+          
+        </li>
+       
+        </div>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-sm-2" type="text" placeholder="Search">
+        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+</body>
+</html>
---
+++ readme.txt
@@ -0,0 +1,28 @@
+
+----------------------------------------------------
+Project: Simple Music Cloud Community System using PHP/MySQLi
+
+About The Project
+
+The songs.com project is Creating for the use of the REST API.this project is work for
+search here user search by song name or artist name or time(in sec).i am also use the inner join for display the data from database to use of the API.
+ 
+
+Features;
+Search by Song name
+Search by artist name
+search by TIME(IN SEC)
+
+The Simple Music Cloud Community System was developed using HTML, PHP/MySQLi, CSS, JavaScript (jQuery),REST API . The source code is fully functional and easy to modify or enhance. Follow the instruction below to have an actual experience using this simple project.
+
+How to Run
+Download the source code and extract the zip file.
+Download or set up any local web server that runs PHP script.
+Open the web-server database and create a new database name it songs_db.
+Import the SQL file located in the database folder of the source code.
+Copy and paste the source code to the location where your local web server accessing your local projects. Example for XAMPP('C:\xampp\htdocs')
+Open a web browser and browse the project. E.g [http://localhost/Songs]
+
+I hope this Songs.com System will help you with what you are looking for, feel free to download and modify the source code.
+
+Thank you!
